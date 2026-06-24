5464a46f192781045b9b38f235972f0198ae7e51
Mugesh Kannan <mkannan2104@gmail.com>
2026-06-25 00:40:52 +0530

    feat: add google-adsense-account metadata to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -82,6 +82,9 @@ export const metadata: Metadata = {
   alternates: {
     canonical: "/",
   },
+  other: {
+    "google-adsense-account": "ca-pub-8628576985544741",
+  },
 };
 
 export default function RootLayout({
